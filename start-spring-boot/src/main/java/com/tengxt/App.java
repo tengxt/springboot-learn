4ba8b34a51056a92c152d5bd75510ca07c7f3174
nsjd <775480627@qq.com>
2019-07-30 17:37:02 +0800

    01.创建Spring Boot项目

---
+++ start-spring-boot/src/main/java/com/tengxt/App.java
@@ -0,0 +1,19 @@
+package com.tengxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration  //开启自动化配置。
+@RestController
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Spring Boot";
+    }
+}
